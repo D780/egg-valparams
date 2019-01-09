bb20e6684d440b30825ecd608f5480d0197ceab1
丁志鹏 <514591280@qq.com>
2019-01-09 16:09:58 +0800

    增加 valparams模块的options参数支持

--- app/extend/context.js
+++ app/extend/context.js
@@ -3,25 +3,37 @@
 module.exports = {
   validate
 };
+
 /**
- * validate data with rules
- *
- * @param  {Object} rules  - Valparams 规则设置 see ./lib/Valparams/wiki.md
- * @param  {Object} [data] - validate target, default to
- *                                            {
-   *                                               params: this.params,
-   *                                               query : this.request.query,
-   *                                               body  : this.request.body
-   *                                             }
+ * ###配置参数格式并进行检测
+ * rules 和 options 查阅 valparams 相关文档说明
+ * @param {Object} rules {pname:{alias,type,required,range:{in,min,max,reg},defValue,allowEmptyStr,desc[,detail]}}
+ * @param {String} rules.pname 参数名，同时也是验证后使用的变量名
+ * @param {String} rules.pname.alias 参数别名，定义该值，前端就用该值传参数来而不是pname
+ * @param {String} rules.pname.type 参数类型
+ * @param {String} rules.pname.required 参数是否必选
+ * @param {String} rules.pname.range 参数范围限制 可用{in,min,max,reg} 分别对应 包含 最小 最大 正则
+ *                                        其中minmax对于不同类型有不同的意思，比如int是值大小，string是字符串长度
+ * @param {String} rules.pname.defValue 参数变量
+ * @param {String} rules.pname.allowEmptyStr 是否允许空串变量 默认不允许， 即 XXXX?YYY= 这种路由 YYY这个参数是否接受，默认这种情况认为没有传该参数
+ * @param {String} rules.pname.desc 参数描述 用于出错返回的提示
+ * @param {Object} options [{choices:[{fields,count,force}],equals:[],compares:[]]
+ * @param {Object} [data] - 参数信息 默认使用请求的 params，query，body 数据如下，有需要可以东西设置
+ *                        {
+ *                          params: this.params,
+ *                          query : this.request.query,
+ *                          body  : this.request.body
+ *                        }
  */
-function validate(rules, data) {
-  data = data || {
+function validate(rules, options, data) {
+  options = options || {};
+  data    = data || {
     params: this.params,
     query : this.request.query,
     body  : this.request.body
   };
   if (rules) {
-    const validater = this.app.Valparams.setParams(data, rules);
+    const validater = this.app.Valparams.setParams(data, rules, options);
     // {
     //   sysID : {alias: 'sid', type: 'int', required: true, desc: '所属系统id'},
     //   page  : {type: 'int', required: false, default: 1, range: {min: 0} desc: '页码'},
