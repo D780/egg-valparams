db36f27dafa88e20ff08f569e617d877f010fb76
丁志鹏 <514591280@qq.com>
2019-04-02 10:47:07 +0800

    test

--- test/valparams.locale.test.js
+++ test/valparams.locale.test.js
@@ -19,7 +19,7 @@ describe('test/valparams.locale.test.js', () => {
       return app.httpRequest()
         .get('/users.json')
         .type('json')
-        .expect(422)
+        .expect(400)
         .expect(res => {
           assert(res.body.code === 'invalid_param');
           assert(res.body.message === 'Validation Failed');
@@ -38,7 +38,7 @@ describe('test/valparams.locale.test.js', () => {
           system  : 'admin',
           redirect: 'http://github.com',
         })
-        .expect(422)
+        .expect(400)
         .expect(res => {
           assert(res.body.code === 'invalid_param');
           assert(res.body.message === 'Validation Failed');
@@ -59,7 +59,7 @@ describe('test/valparams.locale.test.js', () => {
           redirect: 'http://github.com',
           ts      : 'testtest',
         })
-        .expect(422)
+        .expect(400)
         .expect(res => {
           assert(res.body.code === 'invalid_param');
           assert(res.body.message === 'Validation Failed');
